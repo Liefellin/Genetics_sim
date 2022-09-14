a8b7acf3eebfd7310b07eac57a3d68a73591bb31
liefellin <weykerz2004@gmail.com>
2022-09-14 13:55:33 -0400

    RYB fur mixes

--- Tribbles.py
+++ Tribbles.py
@@ -24,7 +24,7 @@ print("Hello, World!")
 # only expressed if spot potential is positive
 # only male/female can breed. sex of child is random 50/50.
 
-phenotypes = dict(FCU="blue", FCR="red", fcb="black", fcw="white", eyu="blue", EYN="brown", sps="spotted", spN="spotless", SCB="black", SCW="white", scr="red", scu="blue", fll="long", FLS="short", CDC="cloudy-eyed", sxx="female", SXY="male")
+phenotypes = dict(FCU="blue", FCR="red", FCY="yellow", fcb="black", fcw="white", eyu="blue", EYN="brown", sps="spotted", SPN="spotless", SCB="black", SCW="white", scr="red", scu="blue", scy="yellow", fll="long", FLS="short", CDC="cloudy-eyed", sxx="female", SXY="male")
 
 
 # 3-letter codes are decided by a two-letter category identifier
@@ -33,19 +33,19 @@ phenotypes = dict(FCU="blue", FCR="red", fcb="black", fcw="white", eyu="blue", E
 # Y for male or C for cloudy
 # dominant traits are capitalized
 
-pheno_list = {"sex": "", "fur_color": "", "fur_length": "", "spots": "", "spot_color": "", "eye_color": ""}
+pheno_list = {"name": "", "sex": "", "fur_color": "", "fur_length": "", "spots": "", "spot_color": "", "eye_color": ""}
 
-codom_scenarios = {"sex": None, "fur_color": "intermediate", "fur_length": None, "spots": None, "spot_color": "equal", "eye_color": None}
+codom_scenarios = {"sex": None, "fur_color": "partial", "fur_length": None, "spots": None, "spot_color": "equal", "eye_color": None}
 
-partial_codom = {("red", "blue") or ("blue", "red"): "purple", ("white", "black") or ("black", "white"): "grey"}
+partial_codom = {("red", "blue") or ("blue", "red"): "purple", ("yellow", "blue") or ("blue", "yellow"): "green", ("red", "yellow") or ("yellow", "red"): "orange", ("white", "black") or ("black", "white"): "grey"}
 
 
-def removebefore(txt, target):
+def remove_before(txt, target):
     txt = txt.split(target)
     return txt[1]
 
 
-def removeafter(txt, target):
+def remove_after(txt, target):
     txt = txt.split(target)
     return txt[0]
 
@@ -58,20 +58,19 @@ def heritage(code_a, code_b):
 
 def expression(gene_code):
     other_attributes = []
-
+    pheno_list["name"] = gene_code["name"]
+    del gene_code["name"]
     for item in gene_code:
         # category like sex or fur color
         potential_codom = ""
         for gene in gene_code[item]:
-            print(gene_code[item])
             # specific expression within that category, like male or red
             if "/" in gene:
-                for i in removebefore(gene, "/").split(","):
+                for i in remove_before(gene, "/").split(","):
                     other_attributes.append(phenotypes[i])
-                gene = removeafter(gene, "/")
-                print(gene[-1])
+                gene = remove_after(gene, "/")
             if gene[-1].isupper():
-                gene=gene.upper()
+                gene = gene.upper()
             if potential_codom != "":
                 if potential_codom == gene:
                     pheno_list[item] = phenotypes[gene]
@@ -83,11 +82,11 @@ def expression(gene_code):
                     if codom_scenarios[item] == "equal":
                         pheno_list[item] = f"{gene} and {potential_codom}"
                     elif codom_scenarios[item] == "partial":
-                        pheno_list[item] = partial_codom[(gene, potential_codom)]
+                        pheno_list[item] = partial_codom[(phenotypes[gene], phenotypes[potential_codom])]
                     else:
                         print("Error: p_codom lookup failed")
                         break
-                        # This is meant to be an unreachable state. If this occurs, then the genecode is most likely invalid.
+                        # This is meant to be an unreachable state. If this occurs, then the gene code is most likely invalid.
             potential_codom = gene
         if pheno_list[item] == "":
             pheno_list[item] = phenotypes[gene_code[item][0]]
@@ -105,23 +104,29 @@ def expression(gene_code):
 # }
 # a longhair blue male tribble with cloudy brown eyes and black spots.
 
-# Next objective: implement breeding.
+
+def breeding_failure():
+    print("The Tribbles had a lot of fun, but no baby")
 
 
-def create_child():
+def create_child(parent1, parent2):
+    if expression(parent1)["sex"] == expression(parent2)["sex"]:
+        return breeding_failure
+    else:
 
-    pass
+        pass
 
 
-Tribbles = {}
-
-Adam = {"sex": ["sxx/CDC", "SXY"], "fur_color": ["FCU", "fcw"], "fur_length": ["fll", "fll"], "spots": ["sps", "sps"], "spot_color": ["scu", "SCB"], "eye_color": ["EYN", "EYN"]}
+Adam = {"name": "Adam", "sex": ["sxx", "SXY"], "fur_color": ["FCU", "fcw"], "fur_length": ["fll", "fll"], "spots": ["sps", "sps"], "spot_color": ["scu", "SCB"], "eye_color": ["EYN", "EYN"]}
 # Adam is a longhair blue male tribble with cloudy brown eyes and black spots.
 
-Eve = {"sex": ["sxx", "sxx"], "fur_color": ["FCU", "fcw"], "fur_length": ["FLS", "fll"], "spots": ["SPN", "sps"], "spot_color": ["scu", "SCB"], "eye_color": ["EYN", "EYN"]}
-# Eve is a shorthair red female tribble with blue eyes.
+Eve = {"name": "Eve", "sex": ["sxx", "sxx"], "fur_color": ["FCY", "FCR"], "fur_length": ["FLS", "FLS"], "spots": ["SPN", "sps"], "spot_color": ["scu", "SCB"], "eye_color": ["EYN", "EYN"]}
+# Eve is a shorthair orange female tribble with blue eyes.
 
-Ctu = {"sex": ["sxx", "SXY"], "fur_color": ["FCU", "FCU"], "fur_length": ["fll", "fll"], "spots": ["SPN", "SPN"], "spot_color": ["scu", "scu"], "eye_color": ["eyu", "eyu"]}
+Skye = {"name": "Skye", "sex": ["sxx/CDC", "SXY"], "fur_color": ["FCU", "FCU"], "fur_length": ["FLS", "fll"], "spots": ["sps", "sps"], "spot_color": ["SCW", "scu"], "eye_color": ["eyu", "eyu"]}
+# Skye is a shorthair blue male tribble with cloudy blue eyes and white spots.
 
-print(expression(Adam))
-
+Tribbles = {"Adam": Adam, "Eve": Eve, "Skye": Skye}
+for tribble in Tribbles:
+    print(expression(Tribbles[tribble]))
+print(expression(Tribbles[tribble]) for tribble in Tribbles)
